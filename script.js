ed31b59d473a95cf168cb8f44e19a9420838f735
25Simone <dantsimone111@gmail.com>
2021-12-14 16:25:00 +0100

    inserimento cartella img e script per le immagini

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const headlinesList = ['Questo è un titolo', 'Questo è un titolo diverso', 'Il
 
 // definisco l'array dei paragrafi
 const paragraphsList = [
+    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+];
+
+// definisco l'array dele immagini
+const imagesList = [
+    'img/img-1.jpg',
+    'img/img-2.jpg',
+    'img/img-3.jpg'
 ];
 
 // dichiaro la variabile dell'index corrente dandogli un valore iniziale di 0
@@ -14,6 +21,7 @@ let currentIndex = 0;
 // definisco le variabili di collegamento al codice html
 const headline = document.querySelector('.box-head');
 const paragraph = document.querySelector('.box-main');
+const image = document.querySelector('.box-img');
 
 // dichiaro le variabili dei due pulsanti
 const buttonNext = document.getElementById('page-next');
@@ -22,6 +30,7 @@ const buttonBack = document.getElementById('page-back');
 // stampo l'output corrispondente al valore inziale dell'index
 headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
 paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+image.src = imagesList[currentIndex];
 
 // definisco l'evento 'click' per il button-back
 buttonBack.addEventListener('click', function() {
@@ -31,6 +40,7 @@ buttonBack.addEventListener('click', function() {
     }
     headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
     paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+    image.src = imagesList[currentIndex];
 })
 
 // definisco l'evento 'click' per il button-next
@@ -41,4 +51,5 @@ buttonNext.addEventListener('click', function() {
     }
     headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
     paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+    image.src = imagesList[currentIndex];
 })
