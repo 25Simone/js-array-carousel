61f9656ad0b61a5ca4a15755af309fe998f52718
25Simone <dantsimone111@gmail.com>
2021-12-14 15:29:43 +0100

    impostazione esercizio e codice script per titoli e paragrafi completato

---
+++ script.js
@@ -0,0 +1,44 @@
+// definisco l'array dei titoli
+const headlinesList = ['Questo è un titolo', 'Questo è un titolo diverso', 'Il terzo titolo' ];
+
+// definisco l'array dei paragrafi
+const paragraphsList = [
+    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+];
+
+// dichiaro la variabile dell'index corrente dandogli un valore iniziale di 0
+let currentIndex = 0;
+
+// definisco le variabili di collegamento al codice html
+const headline = document.querySelector('.box-head');
+const paragraph = document.querySelector('.box-main');
+
+// dichiaro le variabili dei due pulsanti
+const buttonNext = document.getElementById('page-next');
+const buttonBack = document.getElementById('page-back');
+
+// stampo l'output corrispondente al valore inziale dell'index
+headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
+paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+
+// definisco l'evento 'click' per il button-back
+buttonBack.addEventListener('click', function() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = headlinesList.length - 1;
+    }
+    headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
+    paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+})
+
+// definisco l'evento 'click' per il button-next
+buttonNext.addEventListener('click', function() {
+    currentIndex++;
+    if (currentIndex > headlinesList.length - 1) {
+        currentIndex = 0;
+    }
+    headline.innerHTML = `<h1>${headlinesList[currentIndex]}</h1>`;
+    paragraph.innerHTML = `<p>${paragraphsList[currentIndex]}</p>`;
+})
